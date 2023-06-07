17bbdefffe5fe7a3ffa7922832b1f450a243bd60
kimjaehoon <kjh950805@naver.com>
2023-06-08 00:54:42 +0900

    admin_up1

--- src/com/dining/admin/Admin_Page.java
+++ src/com/dining/admin/Admin_Page.java
@@ -16,6 +16,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Component;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 public class Admin_Page {
 
@@ -52,7 +53,9 @@ public class Admin_Page {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(255, 240, 245));
-		frame.setBounds(100, 100, 450, 700);
+		frame.setBounds(100, 100, 540, 960);
+		// 화면사이즈고정
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -60,55 +63,55 @@ public class Admin_Page {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 240, 245));
-		panel.setBounds(0, 0, 434, 661);
+		panel.setBounds(0, 0, 524, 932);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		
 		JLabel lblNewLabel = new JLabel("Admin");
-		lblNewLabel.setFont(new Font("AmeriGarmnd BT", lblNewLabel.getFont().getStyle() | Font.BOLD, lblNewLabel.getFont().getSize() + 50));
+		lblNewLabel.setFont(new Font("Comic Sans Ms", lblNewLabel.getFont().getStyle() | Font.BOLD, lblNewLabel.getFont().getSize() + 50));
 		lblNewLabel.setForeground(new Color(65, 105, 225));
-		lblNewLabel.setBounds(133, 65, 184, 64);
+		lblNewLabel.setBounds(171, 65, 244, 64);
 		panel.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("음식점");
-		btnNewButton.setFont(btnNewButton.getFont().deriveFont(btnNewButton.getFont().getStyle() | Font.BOLD, btnNewButton.getFont().getSize() + 18f));
-		btnNewButton.setForeground(new Color(0, 0, 0));
+		RoundedButton btnNewButton = new RoundedButton("음식점");
+		btnNewButton.setFont(new Font("HY엽서M", Font.BOLD, 55));
 		btnNewButton.setBorderPainted(false);
-		btnNewButton.setBackground(new Color(255, 250, 205));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(33, 234, 160, 160);
+		btnNewButton.setBounds(28, 380, 470, 160);
 		panel.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("<html><body><center>리뷰및<br>평점</center></body></html>");
-		btnNewButton_1.setFont(btnNewButton_1.getFont().deriveFont(btnNewButton_1.getFont().getStyle() | Font.BOLD, btnNewButton_1.getFont().getSize() + 15f));
-		btnNewButton_1.setForeground(new Color(0, 0, 0));
-		btnNewButton_1.setBackground(new Color(224, 255, 255));
+		RoundedButton btnNewButton_1 = new RoundedButton("리뷰및평점");
+		btnNewButton_1.setFont(new Font("HY엽서M", Font.BOLD, 55));
 		btnNewButton_1.setBorderPainted(false);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_1.setBounds(237, 237, 160, 160);
+		btnNewButton_1.setBounds(28, 568, 470, 160);
 		panel.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("회원정보");
-		btnNewButton_2.setFont(btnNewButton_2.getFont().deriveFont(btnNewButton_2.getFont().getStyle() | Font.BOLD, btnNewButton_2.getFont().getSize() + 14f));
-		btnNewButton_2.setForeground(new Color(0, 0, 0));
-		btnNewButton_2.setBackground(new Color(255, 182, 193));
+		RoundedButton btnNewButton_2 = new RoundedButton("회원정보");
+		btnNewButton_2.setFont(new Font("HY엽서M", Font.BOLD, 55));
 		btnNewButton_2.setBorderPainted(false);
-		btnNewButton_2.setBounds(133, 420, 160, 160);
+		btnNewButton_2.setBounds(28, 192, 470, 160);
 		panel.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("");
 		btnNewButton_3.setEnabled(false);
 		btnNewButton_3.setBackground(new Color(65, 105, 225));
 		btnNewButton_3.setBorderPainted(false);
-		btnNewButton_3.setBounds(33, 125, 364, 4);
+		btnNewButton_3.setBounds(28, 128, 470, 4);
 		panel.add(btnNewButton_3);
+		
+		JButton btnNewButton_3_1 = new JButton("");
+		btnNewButton_3_1.setEnabled(false);
+		btnNewButton_3_1.setBorderPainted(false);
+		btnNewButton_3_1.setBackground(new Color(65, 105, 225));
+		btnNewButton_3_1.setBounds(28, 786, 470, 4);
+		panel.add(btnNewButton_3_1);
 	}
-	
 }
